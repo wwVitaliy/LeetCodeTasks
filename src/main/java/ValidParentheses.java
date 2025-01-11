cc66cd25cc8583c6093b84e9fa002fcc00e7cdc9
Vitalii Sh <wwvitaliy@gmail.com>
2025-01-11 15:14:13 +0000

    ValidParentheses init

---
+++ src/main/java/ValidParentheses.java
@@ -0,0 +1,45 @@
+/**
+ * 20. Valid Parentheses
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * <p>
+ * Example 1:
+ * Input: s = "()"
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: s = "()[]{}"
+ * Output: true
+ * <p>
+ * Example 3:
+ * Input: s = "(]"
+ * Output: false
+ * Example 4:
+ * Input: s = "([])"
+ * Output: true
+ * <p>
+ * Constraints:
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ */
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses myClass = new ValidParentheses();
+
+        System.out.println(myClass.isValid("()"));
+        System.out.println(myClass.isValid("()[]{}"));
+        System.out.println(myClass.isValid("(]"));
+        System.out.println(myClass.isValid("([])"));
+    }
+}
