6baa68501383fa8ede941e582ee68074a5ba5b5e
Vitalii Sh <wwvitaliy@gmail.com>
2025-01-12 15:49:01 +0000

    AddBinary init

---
+++ src/main/java/AddBinary.java
@@ -0,0 +1,29 @@
+/**
+ * 67. Add Binary
+ * Given two binary strings a and b, return their sum as a binary string.
+ * <p>
+ * Example 1:
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * <p>
+ * Example 2:
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ * <p>
+ * Constraints:
+ * 1 <= a.length, b.length <= 104
+ * a and b consist only of '0' or '1' characters.
+ * Each string does not contain leading zeros except for the zero itself.
+ */
+public class AddBinary {
+    public String addBinary(String a, String b) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        AddBinary myClass = new AddBinary();
+
+        System.out.println(myClass.addBinary("11", "1"));
+        System.out.println(myClass.addBinary("1010", "1011"));
+    }
+}
