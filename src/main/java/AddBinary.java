340f54749f0aa2277da97e59e51b470575bc8cc1
Vitalii Sh <wwvitaliy@gmail.com>
2025-01-12 17:31:00 +0000

    AddBinaty

--- src/main/java/AddBinary.java
+++ src/main/java/AddBinary.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 67. Add Binary
  * Given two binary strings a and b, return their sum as a binary string.
@@ -17,13 +19,33 @@
  */
 public class AddBinary {
     public String addBinary(String a, String b) {
-        return null;
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int temp = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || temp == 1) {
+            if (i >= 0) {
+                temp += a.charAt(i) - '0';
+            }
+            if (j >= 0) {
+                temp += b.charAt(j) - '0';
+            }
+            sb.append(temp % 2);
+            temp /= 2;
+            i--;
+            j--;
+
+        }
+
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
         AddBinary myClass = new AddBinary();
 
         System.out.println(myClass.addBinary("11", "1"));
-        System.out.println(myClass.addBinary("1010", "1011"));
+        System.out.println(myClass.addBinary("1010", "011"));
     }
 }
