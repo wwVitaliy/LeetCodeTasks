9205b504d374be29026a22432500d9a0c5dc1674
Vitalii Sh <wwvitaliy@gmail.com>
2025-01-27 21:10:55 +0000

    IntersectionOfTwoArrays init

---
+++ src/main/java/IntersectionOfTwoArrays.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/**
+ * 349. Intersection of Two Arrays
+ * Given two integer arrays nums1 and nums2,
+ * return an array of their intersection.
+ * Each element in the result must be unique and you may return the result in any order.
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * Example 2:
+ *
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Explanation: [4,9] is also accepted.
+ *
+ * Constraints:
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ */
+public class IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        return nums1;
+    }
+
+    public static void main(String[] args) {
+        IntersectionOfTwoArrays myClass = new IntersectionOfTwoArrays();
+
+        System.out.println(Arrays.toString(myClass.intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+        System.out.println(Arrays.toString(myClass.intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})));
+    }
+}
