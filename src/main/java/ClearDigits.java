163bed9a0e06dd2213cf7ae4cd2e131f67157c48
Vitalii Sh <wwvitaliy@gmail.com>
2025-02-10 23:01:29 +0000

    ClearDigits V2, V3

--- src/main/java/ClearDigits.java
+++ src/main/java/ClearDigits.java
@@ -1,5 +1,8 @@
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 3174. Clear Digits
@@ -27,31 +30,76 @@ import java.util.Set;
  * The input is generated such that it is possible to delete all digits.
  */
 public class ClearDigits {
+
+    // Variant 1
+//    public String clearDigits(String s) {
+//        char[] chars = s.toCharArray();
+//        Set<Integer> indexesToRemove = new HashSet<>();
+//
+//        for (int i = 0; i < chars.length; i++) {
+//            if (Character.isDigit(chars[i])) {
+//                indexesToRemove.add(i);
+//                for (int j = i - 1; ; j--) {
+//                    if (!indexesToRemove.contains(j)) {
+//                        indexesToRemove.add(j);
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//
+//        StringBuilder result = new StringBuilder();
+//
+//        for (int i = 0; i < chars.length; i++) {
+//            if (!indexesToRemove.contains(i)) {
+//                result.append(chars[i]);
+//            }
+//        }
+//
+//        return result.toString();
+//    }
+
+    // Variant 2
+//    public String clearDigits(String s) {
+//        char[] chars = s.toCharArray();
+//        LinkedList<Character> stack = new LinkedList<>();
+//
+//        for (char aChar : chars) {
+//            if (Character.isAlphabetic(aChar)) {
+//                stack.push(aChar);
+//            } else {
+//                stack.pop();
+//            }
+//        }
+//
+//        StringBuilder result = new StringBuilder();
+//
+//        while (!stack.isEmpty()) {
+//            result.append(stack.pop());
+//        }
+//
+//        return result.reverse().toString();
+//    }
+
+    // Variant 3
     public String clearDigits(String s) {
-        char[] chars = s.toCharArray();
-        Set<Integer> indexesToRemove = new HashSet<>();
+        StringBuilder stringBuilder = new StringBuilder(s);
 
-        for (int i = 0; i < chars.length; i++) {
-            if (Character.isDigit(chars[i])) {
-                indexesToRemove.add(i);
-                for (int j = i - 1; ; j--) {
-                    if (!indexesToRemove.contains(j)) {
-                        indexesToRemove.add(j);
-                        break;
-                    }
+        int charIndex = 0;
+
+        while (charIndex < stringBuilder.length()) {
+            if (Character.isDigit(stringBuilder.charAt(charIndex))) {
+                stringBuilder.deleteCharAt(charIndex);
+                if (charIndex > 0) {
+                    stringBuilder.deleteCharAt(charIndex - 1);
+                    charIndex--;
                 }
+            } else {
+                charIndex++;
             }
         }
 
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < chars.length; i++) {
-            if (!indexesToRemove.contains(i)) {
-                result.append(chars[i]);
-            }
-        }
-
-        return result.toString();
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
